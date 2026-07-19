54fd55111009f41d6f79046f8b445c1eac4e4bff
lauriely lourenço <49760702+laurielylourenco@users.noreply.github.com>
2026-07-19 19:16:21 +0000

    adiciona arquivos hello.rb e work_string.rb com exemplos de saídas

---
+++ hello.rb
@@ -0,0 +1,15 @@
+#puts "Hello, World!"
+#p "Ok I will do it"
+#print "Isso daqui é um teste\n"
+
+
+greetings = "Hello, World! greetings"
+
+#puts greetings
+
+
+def say_flamengo(jogador)
+    puts jogador
+end
+
+say_flamengo "Zico"
---
+++ work_string.rb
@@ -0,0 +1,13 @@
+
+
+simple = 'tmb funciona'
+
+puts simple
+
+first_name = 'Lauri'
+last_name = 'Lourenço'
+
+puts first_name + ' ' + last_name
+
+
+puts "Hello, #{first_name} #{last_name}"
